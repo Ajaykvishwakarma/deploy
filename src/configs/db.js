ef7bad69347aa83ad5121041b57b3cceb2f80807
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-04-20 18:41:33 +0530

    deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-// const connect = () => {
-//   return mongoose.connect(
-//     "mongodb+srv://ajaykv054:ajay12345@cluster0.o8wiv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-//   );
-// };
+const connect = () => {
+  return mongoose.connect(
+    "mongodb+srv://ajaykv054:ajay12345@cluster0.o8wiv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  );
+};
 
-// module.exports = connect;
+module.exports = connect;
 
   // OR
 
-const mongoDB = "mongodb://localhost:27017/apartmentmanagement";
-module.exports = () => mongoose.connect(mongoDB)
+// const mongoDB = "mongodb://localhost:27017/apartmentmanagement";
+// module.exports = () => mongoose.connect(mongoDB)
